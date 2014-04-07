6711d0a1933884be647683d6e9a7d28b6e5ca4e3
Andrew Tayman <dretay@gmail.com>
2014-04-07 09:53:33 -0400

    updating jenkins to save grunt locally rather than trying to install globally

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,6 @@ export PATH=$PATH:/opt/node/node-v0.10.15-linux-x64/bin;
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-npm install -g grunt-cli
+npm install grunt-cli --save-dev
 npm install
-grunt rpm-build
+./node_modules/.bin/grunt rpm-build
