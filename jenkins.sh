5326caec0fbc92667b9cb97a7f8790c30c1d0036
Andrew Tayman <dretay@gmail.com>
2014-04-07 10:03:21 -0400

    fixed path issue (apparently) with sudo npm

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ export PATH=$PATH:/opt/node/node-v0.10.15-linux-x64/bin;
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-sudo npm update -g
+sudo /opt/node/node-v0.10.15-linux-x64/bin/npm update -g
 npm install grunt-cli --save-dev
 npm install
 ./node_modules/.bin/grunt rpm-build
