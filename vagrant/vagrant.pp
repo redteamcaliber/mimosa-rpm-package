9da60cface9e974c9c4020a29358714ce7c756a1
Andrew Tayman <dretay@gmail.com>
2014-04-21 15:00:37 -0400

    updated vagrant with new development overrides

--- vagrant/vagrant.pp
+++ vagrant/vagrant.pp
@@ -1,5 +1,6 @@
 node 'uac.vm.mandiant.com' {
   class { 'uac':
+    uacEnv_uac_mcube_api_url    => "https://mcube.mcirt.mandiant.com",
     installProxy                => true,
     installKeystore             => true,
     installDatabase             => true,
@@ -10,5 +11,10 @@ node 'uac.vm.mandiant.com' {
     configureSupervisord        => false,
     setupDevelopmentEnvironment => true,
     proxyStaticFolder           => "/opt/web/apps/uac/dist/client",
+    uacEnv_sso_auth_url         => "https://loginstage.mandiant.com/api/auth",
+    uacEnv_sso_login_url        => "https://loginstage.mandiant.com",
+    uacEnv_sso_refresh_url      => "https://loginstage.mandiant.com/refresh",
+    uacEnv_sso_logout_url       => "https://loginstage.mandiant.com/logout",
+    uacEnv_sso_unauth_url       => "https://loginstage.mandiant.com/unauth"
   }
 }
